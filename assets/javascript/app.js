aa941410914373dab915e6b178f250f09b65c8ab
Prerit Kumar Jha <preritkrjha@gmail.com>
2022-10-09 12:17:15 +0530

    Added scroll to top button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,21 @@ $(".change").on("click",function(){
     $(".change").text("Light");
   }
 });
+
+let backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+  backToTopButtonOffset();
+};
+
+function backToTopButtonOffset() {
+  if (document.documentElement.scrollTop > 40) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+function scrollBackToTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
